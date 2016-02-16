56e75feacf0d882fed6a535703f738b73538ec68
Sakul Jaruthanaset <miolynet@perfenterprise.com>
2016-02-16 15:41:59 +0700

    Setup the test project's references.
    Signed-off-by: Sakul Jaruthanaset <miolynet@perfenterprise.com>

--- MainSolution/test/MindSageWeb.Specs/Adder.feature
+++
@@ -1,5 +0,0 @@
-﻿Feature: Adder
-
-Scenario: Add 2 int
-	Given 2 + 2
-	Then it should be 4
---
+++ MainSolution/test/MindSageWeb.Specs/Specs/Hooks.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace MindSageWeb.Specs.Specs
+{
+    [Binding]
+    public sealed class Hooks
+    {
+        // For additional details on SpecFlow hooks see http://go.specflow.org/doc-hooks
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+        }
+
+        [BeforeScenarioBlock("mock")]
+        public void BeforeScenarioBlock()
+        {
+            if (ScenarioContext.Current.CurrentScenarioBlock == ScenarioBlock.Given)
+            {
+                var mocks = new Moq.MockRepository(Moq.MockBehavior.Strict);
+                ScenarioContext.Current.Set(mocks);
+            }
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+        }
+    }
+}
