fb7d7fa69cff609b12185546057330cbe2cd6e48
Anawat <m.anawat@outlook.com>
2016-02-20 01:48:48 +0700

    add Lesson page's route and edit sass
    Signed-off-by: Anawat <m.anawat@outlook.com>

--- MainSolution/src/MindSageWeb/scripts/app.ts
+++ MainSolution/src/MindSageWeb/scripts/app.ts
@@ -1,8 +1,29 @@
 ﻿angular.module('app', ['ui.router'])
     .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider: angular.ui.IStateProvider, $urlRouterProvider: angular.ui.IUrlRouterProvider) {
-        $stateProvider.state('app', {
-            url: '/app',
-            template: '<h2>Hello?</h2>'
-        });
-        $urlRouterProvider.otherwise('/app');
+        $stateProvider
+
+            .state('app', {
+                url: '/app',
+                templateUrl: 'tmpl/layout.html'
+            })
+
+            .state('app.main', {
+                url: '/main',
+                views: {
+                    'mainContent': {
+                        templateUrl: 'tmpl/lesson_layout.html'
+                    }
+                }
+            })
+
+            .state('app.main.lesson', {
+                url: '/lesson/:lessonid',
+                views: {
+                    'lessonContent': {
+                        templateUrl: 'tmpl/lesson.html'
+                    }
+                }
+            })
+            ;
+        $urlRouterProvider.otherwise('/app/main/lesson/111');
     }]);
