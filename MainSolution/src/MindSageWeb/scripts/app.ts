b92c0e0f3301d82a2226cf5b1c066c7583b641c9
Anawat <m.anawat@outlook.com>
2016-02-23 15:42:37 +0700

    Add All Route
    Signed-off-by: Anawat <m.anawat@outlook.com>

--- MainSolution/src/MindSageWeb/scripts/app.ts
+++ MainSolution/src/MindSageWeb/scripts/app.ts
@@ -17,10 +17,82 @@
             })
 
             .state('app.main.lesson', {
-                url: '/lesson/:lessonid',
+                url: '/lesson/:lessonId',
                 views: {
                     'lessonContent': {
-                        templateUrl: 'tmpl/lesson.html'
+                        templateUrl: 'tmpl/lesson_student.html'
+                    }
+                }
+            })
+
+            .state('app.main.details', {
+                url: '/details',
+                views: {
+                    'lessonContent': {
+                        templateUrl: 'tmpl/lesson_teacher.html'
+                    }
+                }
+            })
+
+            .state('app.course', {
+                url: '/course/:courseId',
+                views: {
+                    'mainContent': {
+                        templateUrl: 'tmpl/course_layout.html'
+                    }
+                }
+            })
+
+            .state('app.course.nitification', {
+                url: '/nitification',
+                views: {
+                    'courseContent': {
+                        templateUrl: 'tmpl/notification.html'
+                    }
+                }
+            })
+
+            .state('app.course.jounal', {
+                url: '/jounal/:userid',
+                views: {
+                    'courseContent': {
+                        templateUrl: 'tmpl/jounal.html'
+                    }
+                }
+            })
+
+            .state('app.course.coursemap', {
+                url: '/coursemap',
+                views: {
+                    'courseContent': {
+                        templateUrl: 'tmpl/coursemap.html'
+                    }
+                }
+            })
+
+            .state('app.course.friendlist', {
+                url: '/friendlist',
+                views: {
+                    'courseContent': {
+                        templateUrl: 'tmpl/friendlist.html'
+                    }
+                }
+            })
+
+            .state('app.course.studentlist', {
+                url: '/studentlist',
+                views: {
+                    'courseContent': {
+                        templateUrl: 'tmpl/studentlist.html'
+                    }
+                }
+            })
+
+            .state('app.course.config', {
+                url: '/setting',
+                views: {
+                    'courseContent': {
+                        templateUrl: 'tmpl/setting.html'
                     }
                 }
             })
