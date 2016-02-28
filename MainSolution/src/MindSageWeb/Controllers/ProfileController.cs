52d71fa2c349a1853fca210380698824c2751910
Sakul Jaruthanaset <miolynet@perfenterprise.com>
2016-02-28 11:04:47 +0700

    Add new profile API (incomplete).
    Signed-off-by: Sakul Jaruthanaset <miolynet@perfenterprise.com>

---
+++ MainSolution/src/MindSageWeb/Controllers/ProfileController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNet.Mvc;
+using MindSageWeb.Repositories;
+using MindSageWeb.Repositories.Models;
+using System;
+using System.Linq;
+
+
+namespace MindSageWeb.Controllers
+{
+    /// <summary>
+    /// Profile API
+    /// </summary>
+    [Route("api/[controller]")]
+    public class ProfileController : Controller
+    {
+        // PUT: api/profile/{user-id}
+        [HttpPut]
+        [Route("like")]
+        public void Put(string id, UpdateProfileRequest body)
+        {
+            // TODO: Not implemented
+            throw new NotImplementedException();
+        }
+
+        //// GET: api/profile
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET: api/profile/5
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST: api/profile
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT: api/profile/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE: api/profile/5
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
