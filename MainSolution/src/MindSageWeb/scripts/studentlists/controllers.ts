d80067d52700e5278f96202de0814ffdacfcc0be
Genkung <parada@perfenterprise.com>
2016-02-29 18:46:56 +0700

    Implement Student List Friend Request Button Change Friend Request to Friend Request sent when chick.
    Signed-off-by: Genkung <parada@perfenterprise.com>

--- MainSolution/src/MindSageWeb/scripts/studentlists/controllers.ts
+++ MainSolution/src/MindSageWeb/scripts/studentlists/controllers.ts
@@ -2,12 +2,15 @@
     'use strict';
 
     class studentlistsController {
+        private nStatus: number;
         static $inject = ['$scope', 'list', 'classRoomId', 'app.studentlists.StudentListService'];
-        constructor(private $scope, public list, public classRoomId: string, private listsSvc: app.studentlists.StudentListService) {
+        constructor(private $scope, public list : any[], public classRoomId: string, private listsSvc: app.studentlists.StudentListService) {
         }
 
-        public SendFriendRequest(SendtoId: string) {
-            this.listsSvc.SendFriendRequest(SendtoId, null, false);
+        public SendFriendRequest(friendObj: any) {
+            var EditIndex = this.list.indexOf(friendObj);
+            this.list[EditIndex].Status = 0;
+            this.listsSvc.SendFriendRequest(friendObj.UserProfileId, null, false);
         }
 
         public ConfirmFriendRequest(SendtoId: string, requestId: string) {
