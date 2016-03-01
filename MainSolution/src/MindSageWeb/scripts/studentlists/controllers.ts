2e853b2a5a7fe44246a53cc633737dcd3c63df74
Genkung <parada@perfenterprise.com>
2016-03-01 19:16:08 +0700

    Implement Confirm and Delete Request change stage.
    Signed-off-by: Genkung <parada@perfenterprise.com>

--- MainSolution/src/MindSageWeb/scripts/studentlists/controllers.ts
+++ MainSolution/src/MindSageWeb/scripts/studentlists/controllers.ts
@@ -4,7 +4,7 @@
     class studentlistsController {
         private nStatus: number;
         static $inject = ['$scope', 'list', 'classRoomId', 'app.studentlists.StudentListService'];
-        constructor(private $scope, public list : any[], public classRoomId: string, private listsSvc: app.studentlists.StudentListService) {
+        constructor(private $scope, public list: any[], public classRoomId: string, private listsSvc: app.studentlists.StudentListService) {
         }
 
         public SendFriendRequest(friendObj: any) {
@@ -13,14 +13,20 @@
             this.listsSvc.SendFriendRequest(friendObj.UserProfileId, null, false);
         }
 
-        public ConfirmFriendRequest(SendtoId: string, requestId: string) {
-            this.listsSvc.SendFriendRequest(SendtoId, requestId, true);
+        public ConfirmFriendRequest(friendObj: any) {
+            var EditIndex = this.list.indexOf(friendObj);
+            this.list[EditIndex].Status = 2;
+            this.listsSvc.SendFriendRequest(friendObj.UserProfileId, friendObj.RequestId, true);
         }
 
-        public DeleteFriendRequest(SendtoId: string, requestId: string) {
-            this.listsSvc.SendFriendRequest(SendtoId, requestId, false);
-        }
+        public DeleteFriendRequest(friendObj: any) {
+            var EditIndex = this.list.indexOf(friendObj);
+            this.list[EditIndex].Status = 3;
+            this.listsSvc.SendFriendRequest(friendObj.UserProfileId, friendObj.RequestId, false);
 
+        }
+        //UserProfileId
+        //RequestId
     }
 
     angular
