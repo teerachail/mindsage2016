cdb9c7df76b2932c70b28263fc25302581a06ee3
Teerachai <teerachai@perfenterprise.com>
2016-03-10 15:29:56 +0700

    Add Foundation for Apps to WebAppSolution/MindSageWeb.
    Signed-off-by: Teerachai <teerachai@perfenterprise.com>

---
+++ WebAppSolution/src/MindSageWeb/scripts/typings/browser.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="browser\ambient\angular\angular.d.ts" />
+/// <reference path="browser\ambient\fastclick\fastclick.d.ts" />
+/// <reference path="browser\ambient\foundation-sites\foundation-sites.d.ts" />
+/// <reference path="browser\ambient\jquery\jquery.d.ts" />
---
+++ WebAppSolution/src/MindSageWeb/scripts/typings/main.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="main\ambient\angular\angular.d.ts" />
+/// <reference path="main\ambient\fastclick\fastclick.d.ts" />
+/// <reference path="main\ambient\foundation-sites\foundation-sites.d.ts" />
+/// <reference path="main\ambient\jquery\jquery.d.ts" />
