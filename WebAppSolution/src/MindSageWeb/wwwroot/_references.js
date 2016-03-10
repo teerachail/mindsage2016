33775c1fa70e2b218be74f876adbe8c4c31ea713
Teerachai <teerachai@perfenterprise.com>
2016-03-10 19:01:02 +0700

    Just update script references for WebAppSolution.
    Signed-off-by: Teerachai <teerachai@perfenterprise.com>

--- WebAppSolution/src/MindSageWeb/wwwroot/_references.js
+++ WebAppSolution/src/MindSageWeb/wwwroot/_references.js
@@ -13,7 +13,6 @@
 /// <reference path="lib/fastclick/lib/fastclick.js" />
 /// <reference path="lib/foundation-apps/dist/js/foundation-apps.js" />
 /// <reference path="lib/foundation-apps/dist/js/foundation-apps-templates.js" />
-/// <reference path="lib/foundation-datepicker/js/foundation-datepicker.js" />
 /// <reference path="lib/hammerjs/hammer.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
 /// <reference path="lib/jquery-validation/dist/jquery.validate.js" />
