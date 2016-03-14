25a507413fe2bd365bd47ccafca9c467bb17408c
Sakul Jaruthanaset <miolynet@perfenterprise.com>
2016-03-14 12:00:36 +0700

    Add MongoDB and its configuration complete.
    Signed-off-by: Sakul Jaruthanaset <miolynet@perfenterprise.com>

---
+++ WebAppSolution/src/MindSageWeb/AppConfigOptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MindSageWeb
+{
+    public class AppConfigOptions
+    {
+        #region Properties
+
+        public string PrimaryDBConnectionString { get; set; }
+        public string PrimaryDBName { get; set; }
+        public string GoogleClinetId { get; set; }
+        public string GoogleClientSecret { get; set; }
+
+        #endregion Properties
+    }
+}
---
+++ WebAppSolution/src/MindSageWeb/MongoAccess/MongoUtil.cs
@@ -0,0 +1,67 @@
+﻿using MongoDB.Driver;
+
+namespace MindSageWeb.MongoAccess
+{
+    public class MongoUtil
+    {
+        #region Fields
+
+        private static IMongoClient _client;
+        private static IMongoDatabase _database;
+        private static MongoUtil _instance;
+
+        #endregion Fields
+
+        #region Properties
+
+        public static MongoUtil Instance
+        {
+            get
+            {
+                if (_instance == null) _instance = new MongoUtil();
+                return _instance;
+            }
+
+            set
+            {
+                _instance = value;
+            }
+        }
+
+        #endregion Properties
+
+        #region Constructors
+
+        private MongoUtil()
+        {
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        /// <summary>
+        /// Initialize database's connection
+        /// </summary>
+        /// <param name="appConfig">App configuration</param>
+        public void Initialize(AppConfigOptions appConfig)
+        {
+            var connectionString = appConfig.PrimaryDBConnectionString;
+            var dbName = appConfig.PrimaryDBName;
+            _client = new MongoClient(connectionString);
+            _database = _client.GetDatabase(dbName);
+        }
+
+        /// <summary>
+        /// ดึงข้อมูลจากตาราง
+        /// </summary>
+        /// <typeparam name="T">ข้อมูลที่ทำงานด้วย</typeparam>
+        /// <param name="tableName">ชื่อตาราง</param>
+        public IMongoCollection<T> GetCollection<T>(string tableName)
+        {
+            return _database.GetCollection<T>(tableName);
+        }
+
+        #endregion Methods
+    }
+}
