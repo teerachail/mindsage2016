f2f59d8e35a3e052140d243c4f01f7847f45adc9
Sakul Jaruthanaset <miolynet@perfenterprise.com>
2016-03-16 19:27:42 +0700

    Implement display Angular routing complete.
    Signed-off-by: Sakul Jaruthanaset <miolynet@perfenterprise.com>

--- WebAppSolution/src/MindSageWeb/Controllers/MyController.cs
+++ WebAppSolution/src/MindSageWeb/Controllers/MyController.cs
@@ -5,33 +5,15 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 using Microsoft.AspNet.Authorization;
 
-// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace MindSageWeb.Controllers
 {
     [Authorize]
     public class MyController : Controller
     {
-        private MyCourseController _myCourseCtrl;
-
-        public MyController(MyCourseController myCourseCtrl)
-        {
-            _myCourseCtrl = myCourseCtrl;
-        }
-
         // GET: /<controller>/
         public IActionResult Index()
         {
-            ViewBag.HaveAnyCourse = getAllUserCourses().Any();
             return View();
-        }
-
-        private IEnumerable<string> getAllUserCourses()
-        {
-            var userId = User.Identity.IsAuthenticated ? User.Identity.Name : string.Empty;
-            var qry = _myCourseCtrl.GetAllCourses(userId)
-                .Select(it => it.CourseCatalogId);
-            return qry;
         }
     }
 }
