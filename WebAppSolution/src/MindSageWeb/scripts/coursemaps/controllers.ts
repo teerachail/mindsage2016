2069d8955e37da6b8484ae9a76c369bdb2d3861d
Sakul Jaruthanaset <miolynet@perfenterprise.com>
2016-03-17 15:22:25 +0700

    Fix bug in the Course map page complete.
    Signed-off-by: Sakul Jaruthanaset <miolynet@perfenterprise.com>

--- WebAppSolution/src/MindSageWeb/scripts/coursemaps/controllers.ts
+++ WebAppSolution/src/MindSageWeb/scripts/coursemaps/controllers.ts
@@ -4,10 +4,43 @@
     class CourseMapController {
 
         private userProfile: any;
+        private content;
+        private status;
+        private isWaittingForGetCourseMapContent: boolean;
+        private isPrepareCourseMapContentComplete: boolean;
 
-        static $inject = ['$scope','$state', 'content', 'status', 'app.shared.ClientUserProfileService'];
-        constructor(private $scope, private $state, public content, public status, private userSvc: app.shared.ClientUserProfileService) {
-            this.userProfile = userSvc.GetClientUserProfile();
+        static $inject = ['$scope', '$state', '$q', '$stateParams', 'waitRespondTime', 'app.shared.ClientUserProfileService', 'app.coursemaps.CourseMapService'];
+        constructor(private $scope, private $state, private $q, private $stateParams, private waitRespondTime, private userSvc: app.shared.ClientUserProfileService, private coursemapSvc: app.coursemaps.CourseMapService) {
+            this.prepareUserprofile();
+        }
+
+        private prepareUserprofile(): void {
+            if (!this.userSvc.IsPrepareAllUserProfileCompleted()) {
+                setTimeout(it => this.prepareUserprofile(), this.waitRespondTime);
+                return;
+            }
+
+            this.userProfile = this.userSvc.GetClientUserProfile();
+            this.prepareCourseMapContents();
+        }
+        private prepareCourseMapContents(): void {
+            var shouldRequestJournalContent = !this.isPrepareCourseMapContentComplete && !this.isWaittingForGetCourseMapContent;
+            if (shouldRequestJournalContent) {
+                this.isWaittingForGetCourseMapContent = true;
+                this.$q.all([
+                    this.coursemapSvc.GetContent(this.$stateParams.classRoomId),
+                    this.coursemapSvc.GetLessonStatus(this.$stateParams.classRoomId),
+                ]).then(data => {
+                    this.content = data[0];
+                    this.status = data[1];
+                    this.isWaittingForGetCourseMapContent = false;
+                    this.isPrepareCourseMapContentComplete = true;
+                }, error => {
+                    console.log('Load journal content failed, retrying ...');
+                    this.isWaittingForGetCourseMapContent = false;
+                    setTimeout(it=> this.prepareCourseMapContents(), this.waitRespondTime);
+                });
+            }
         }
 
         public HaveAnyComments(lessonId: string): boolean {
