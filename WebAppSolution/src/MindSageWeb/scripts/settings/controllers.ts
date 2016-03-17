aea9490391d72c6004bdbb15f2cc92f64516843f
Sakul Jaruthanaset <miolynet@perfenterprise.com>
2016-03-17 18:54:26 +0700

    Fix bugs in the Setting page complete.
    Signed-off-by: Sakul Jaruthanaset <miolynet@perfenterprise.com>

--- WebAppSolution/src/MindSageWeb/scripts/settings/controllers.ts
+++ WebAppSolution/src/MindSageWeb/scripts/settings/controllers.ts
@@ -2,51 +2,41 @@
     'use strict';
     
     class SettingController {
-        
-        private userInfo;
-        public ClassName: string;
-        public CurrentStudentCode: string;
 
-        static $inject = ['$scope', '$state', 'app.settings.ProfileService', 'courseInfo', 'app.shared.ClientUserProfileService', 'app.shared.GetProfileService'];
-        constructor(private $scope, private $state, private profileSvc: app.settings.ProfileService, public courseInfo, private clientProfileSvc: app.shared.ClientUserProfileService, private getProfile: app.shared.GetProfileService) {
+        private userInfo: shared.ClientUserProfile;
+
+        static $inject = ['$scope', '$state', 'waitRespondTime', 'app.settings.ProfileService', 'app.shared.ClientUserProfileService'];
+        constructor(private $scope, private $state, private waitRespondTime, private profileSvc: app.settings.ProfileService, private clientProfileSvc: app.shared.ClientUserProfileService) {
+            this.userInfo = new shared.ClientUserProfile();
+            this.prepareUserprofile();
+        }
+
+        private prepareUserprofile(): void {
+            if (!this.clientProfileSvc.IsPrepareAllUserProfileCompleted()) {
+                setTimeout(it => this.prepareUserprofile(), this.waitRespondTime);
+                return;
+            }
+
             this.userInfo = this.clientProfileSvc.GetClientUserProfile();
-            this.ClassName = this.courseInfo.ClassName;
-            this.CurrentStudentCode = this.courseInfo.CurrentStudentCode;
         }
-
-        public OpenStudentListPage(name: string) {
-            this.$state.go("app.course.teacherlist", { 'classRoomId': this.userInfo.CurrentClassRoomId }, { inherit: false });
-        }
-
+        
         public UpdateProfile(name: string, schoolName: string, isPrivate: boolean, isReminderOnceTime: boolean) {
             if (name != null && name != "") this.profileSvc.UpdateProfile(name, schoolName, isPrivate, isReminderOnceTime);
         }
 
         public UpdateCoursee(ClassName: string, ChangedStudentCode: string, BeginDate: Date) {
-            if (this.courseInfo.ClassName == ClassName) ClassName = null;
-            if (this.courseInfo.CurrentStudentCode == ChangedStudentCode) ChangedStudentCode = null;
+            if (this.userInfo.ClassName == ClassName) ClassName = null;
+            if (this.userInfo.CurrentStudentCode == ChangedStudentCode) ChangedStudentCode = null;
             if (ClassName != null || ChangedStudentCode != null) this.profileSvc.UpdateCourse(ClassName, ChangedStudentCode, BeginDate);
         }
 
         public DeleteCourse() {
-            this.profileSvc.DeleteCourse(this.courseInfo.ClassRoomId);
+            this.profileSvc.DeleteCourse(this.userInfo.CurrentClassRoomId);
         }
+
         public StudenMessageEdit(Message: string) {
             this.profileSvc.StudenMessageEdit(Message);
         }
-        public GetAllCourse() {
-            this.getProfile.GetAllCourse();
-        }
-        public GetNotificationNumber() {
-            this.getProfile.GetNotificationNumber();
-        }
-        public GetNotificationContent() {
-            this.getProfile.GetNotificationContent();
-        }
-        public GetLike() {
-            this.getProfile.GetLike();
-        }
-        
     }
 
     angular
