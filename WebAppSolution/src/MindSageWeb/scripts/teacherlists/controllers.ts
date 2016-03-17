33839c890fede412d8eee74d181cb7fb587f713f
Sakul Jaruthanaset <miolynet@perfenterprise.com>
2016-03-17 17:50:54 +0700

    Fix bugs in the TeacherList page complete.
    Signed-off-by: Sakul Jaruthanaset <miolynet@perfenterprise.com>

--- WebAppSolution/src/MindSageWeb/scripts/teacherlists/controllers.ts
+++ WebAppSolution/src/MindSageWeb/scripts/teacherlists/controllers.ts
@@ -2,10 +2,42 @@
     'use strict';
 
     class teacherlistsController {
-        public targetId: any;
 
-        static $inject = ['$scope', 'list', 'classRoomId', 'app.teacherlists.TeacherListService'];
-        constructor(private $scope, public list: any[], public classRoomId: string, private teacherlistsSvc: app.teacherlists.TeacherListService) {
+        public targetId: any;
+        private list: any[] = [];
+        private classRoomId: string;
+        private isWaittingForGetTeacherListContent: boolean;
+        private isPrepareTeacherListContentComplete: boolean;
+
+        static $inject = ['$scope', 'waitRespondTime', '$stateParams', 'app.shared.ClientUserProfileService', 'app.teacherlists.TeacherListService'];
+        constructor(private $scope, private waitRespondTime, private $stateParams, private userprofileSvc: app.shared.ClientUserProfileService, private teacherlistsSvc: app.teacherlists.TeacherListService) {
+            this.classRoomId = $stateParams.classRoomId;
+            this.prepareUserprofile();
+        }
+
+        private prepareUserprofile(): void {
+            if (!this.userprofileSvc.IsPrepareAllUserProfileCompleted()) {
+                setTimeout(it => this.prepareUserprofile(), this.waitRespondTime);
+                return;
+            }
+
+            this.prepareTeacherListContents();
+        }
+
+        private prepareTeacherListContents(): void {
+            var shouldRequestLessonContent = !this.isPrepareTeacherListContentComplete && !this.isWaittingForGetTeacherListContent;
+            if (shouldRequestLessonContent) {
+                this.isWaittingForGetTeacherListContent = true;
+                this.teacherlistsSvc.GetStudentList(this.classRoomId).then(respond => {
+                    if (respond != null) this.list = respond;
+                    this.isWaittingForGetTeacherListContent = false;
+                    this.isPrepareTeacherListContentComplete = true;
+                }, error => {
+                    console.log('Load TeacherList content failed, retrying ...');
+                    this.isWaittingForGetTeacherListContent = false;
+                    setTimeout(it => this.prepareTeacherListContents(), this.waitRespondTime);
+                });
+            }
         }
 
         public targetStd(Std: any) {
