bacef9faf5a28d69d23fbe3f7457c6755fcc1526
Sakul Jaruthanaset <miolynet@perfenterprise.com>
2016-03-27 02:43:54 +0700

    Implement API for preview course catalog complete (untested).
    Signed-off-by: Sakul Jaruthanaset <miolynet@perfenterprise.com>

---
+++ WebManagementPortal/WebManagementPortal/Controllers/CourseCatalogController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using WebManagementPortal.EF;
+using System.Data.Entity;
+
+namespace WebManagementPortal.Controllers
+{
+    [RoutePrefix("api/coursecatalog")]
+    public class CourseCatalogController : ApiController
+    {
+        // GET: api/CourseCatalog/{course-catalog-id}
+        [Route("{id}")]
+        public async Task<Models.CourseCatalog> Get(int id)
+        {
+            CourseCatalog courseCatalog;
+            using (var dctx = new EF.MindSageDataModelsContainer())
+            {
+                courseCatalog = await dctx.CourseCatalogs
+                    .Include("Semesters.Units.Lessons.Advertisements")
+                    .Include("Semesters.Units.Lessons.TopicOfTheDays")
+                    .FirstOrDefaultAsync(it => it.Id == id);
+            }
+            if (courseCatalog == null) return null;
+
+            var semesterQry = courseCatalog.Semesters.Where(it => !it.RecLog.DeletedDate.HasValue);
+            var unitQry = semesterQry.SelectMany(it => it.Units).Where(it => !it.RecLog.DeletedDate.HasValue);
+            var lessonQry = unitQry.SelectMany(it => it.Lessons).Where(it => !it.RecLog.DeletedDate.HasValue);
+
+            var lessonIdRunner = 1;
+            var lessons = lessonQry.Select(it => new Models.CourseCatalog.Lesson
+            {
+                id = it.Id.ToString(),
+                Order = lessonIdRunner++
+                //Contents
+            });
+
+            var unitIdRunner = 1;
+            var units = unitQry.Select(it => new Models.CourseCatalog.Unit
+            {
+                id = it.Id.ToString(),
+                Title = it.Title,
+                Description = it.Description,
+                Order = unitIdRunner++,
+                Lessons = lessons
+            });
+
+            var semesterNameRunner = (byte)65;
+            var semesters = semesterQry.Select(it => new Models.CourseCatalog.Semester
+            {
+                id = it.Id.ToString(),
+                Title = it.Title,
+                Description = it.Description,
+                Units = units,
+                Name = string.Format("{0}", (char)semesterNameRunner++)
+            });
+
+            var result = new Models.CourseCatalog
+            {
+                id = courseCatalog.Id.ToString(),
+                Advertisements = courseCatalog.Advertisements.Split(new string[] { "#;" }, StringSplitOptions.RemoveEmptyEntries),
+                CreatedDate = courseCatalog.RecLog.CreatedDate,
+                DeletedDate = courseCatalog.RecLog.DeletedDate,
+                DescriptionImageUrl = courseCatalog.DescriptionImageUrl,
+                FullDescription = courseCatalog.FullDescription,
+                Grade = courseCatalog.Grade,
+                GroupName = courseCatalog.GroupName,
+                PriceUSD = courseCatalog.PriceUSD,
+                Semesters = semesters,
+                Series = courseCatalog.Series,
+                SideName = courseCatalog.SideName,
+                Title = courseCatalog.Title,
+            };
+            return result;
+        }
+    }
+}
---
+++ WebManagementPortal/WebManagementPortal/Models/CourseCatalog.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebManagementPortal.Models
+{
+    public class CourseCatalog
+    {
+        #region Properties
+
+        public string id { get; set; }
+        public string GroupName { get; set; }
+        public string Grade { get; set; }
+        public string SideName { get; set; }
+        public double PriceUSD { get; set; }
+        public string Series { get; set; }
+        public string Title { get; set; }
+        public string FullDescription { get; set; }
+        public string DescriptionImageUrl { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? DeletedDate { get; set; }
+
+        public IEnumerable<Semester> Semesters { get; set; }
+        public IEnumerable<string> Advertisements { get; set; }
+
+        #endregion Properties
+
+        public class Semester
+        {
+            #region Properties
+
+            public string id { get; set; }
+            public string Name { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+
+            public IEnumerable<Unit> Units { get; set; }
+
+            #endregion Properties
+        }
+
+        public class Unit
+        {
+            #region Properties
+
+            public string id { get; set; }
+            public int Order { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+
+            public IEnumerable<Lesson> Lessons { get; set; }
+
+            #endregion Properties
+        }
+
+        public class Lesson
+        {
+            #region Properties
+
+            public string id { get; set; }
+            public int Order { get; set; }
+            public IEnumerable<LessonContent> Contents { get; set; }
+
+            #endregion Properties
+        }
+
+        public class LessonContent
+        {
+            #region Properties
+
+            public string ImageUrl { get; set; }
+            public string Description { get; set; }
+
+            #endregion Properties
+        }
+    }
+}
