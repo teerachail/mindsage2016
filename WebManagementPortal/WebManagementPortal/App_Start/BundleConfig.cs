4342c9bf73f386d88bc3d51e7f18c9e3469405dc
Sakul Jaruthanaset <miolynet@perfenterprise.com>
2016-03-26 17:18:12 +0700

    Add AngularJs.
    Signed-off-by: Sakul Jaruthanaset <miolynet@perfenterprise.com>

--- WebManagementPortal/WebManagementPortal/App_Start/BundleConfig.cs
+++ WebManagementPortal/WebManagementPortal/App_Start/BundleConfig.cs
@@ -17,7 +17,8 @@ namespace WebManagementPortal
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/modernizr-*",
+                        "~/Scripts/angular.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
