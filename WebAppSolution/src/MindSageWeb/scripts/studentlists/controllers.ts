06701230f2df03001ab2d9657e96092b7cbef0ed
Anawat <m.anawat@outlook.com>
2016-03-29 10:54:35 +0700

    Add order and search button to Student list

--- WebAppSolution/src/MindSageWeb/scripts/studentlists/controllers.ts
+++ WebAppSolution/src/MindSageWeb/scripts/studentlists/controllers.ts
@@ -4,11 +4,28 @@
     class studentlistsController {
 
         private classRoomId: string;
+        public keyword: string;
         private friendList: any[] = [];
+        private searchList: any[] = [];
+        private searching: boolean;
 
-        static $inject = ['$scope', '$stateParams', 'app.shared.ClientUserProfileService'];
-        constructor(private $scope, private $stateParams, private userSvc: app.shared.ClientUserProfileService) {
+        static $inject = ['$scope', '$stateParams', 'app.shared.ClientUserProfileService', 'app.studentlists.StudentListService'];
+        constructor(private $scope, private $stateParams, private userSvc: app.shared.ClientUserProfileService, private searchSvc: app.studentlists.StudentListService) {
             this.classRoomId = $stateParams.classRoomId;
+            this.searching = false;
+        }
+
+        public searchNewFriend() {
+            if (this.keyword == null || this.keyword.length == 0) return;
+            this.searching = true;
+            this.searchSvc.SearchFriendRequest(this.classRoomId, this.keyword).then(respond => {
+                this.searchList = respond;
+            });
+        }
+
+        public cancelSearch() {
+            this.keyword = "";
+            this.searching = false;
         }
     }
 
