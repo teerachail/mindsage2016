7f7eb52a018f8555034e03fd54234f839102f6a4
Sakul Jaruthanaset <miolynet@perfenterprise.com>
2016-04-21 22:55:46 +0700

    Clear old specs.
    Signed-off-by: Sakul Jaruthanaset <miolynet@perfenterprise.com>

--- WebAppSolution/test/MindSageWeb.Specs/Specs/Steps/Teacher_Delete_CourseSteps.cs
+++ WebAppSolution/test/MindSageWeb.Specs/Specs/Steps/Teacher_Delete_CourseSteps.cs
@@ -41,5 +41,48 @@ namespace MindSageWeb.Specs.Steps
                 ClassRoomId = classRoomId
             });
         }
+
+        [Then(@"System upsert ClassRoom with JSON format is")]
+        public void ThenSystemUpsertClassRoomWithJSONFormatIs(string multilineText)
+        {
+            var expected = JsonConvert.DeserializeObject<ClassRoom>(multilineText);
+
+            var mockClassRoomRepo = ScenarioContext.Current.Get<Mock<IClassRoomRepository>>();
+            mockClassRoomRepo.Verify(it => it.UpsertClassRoom(It.Is<ClassRoom>(actual =>
+                JsonConvert.SerializeObject(actual) == JsonConvert.SerializeObject(expected)
+            )));
+        }
+
+        [Then(@"System upsert ClassCalendar with JSON format is")]
+        public void ThenSystemUpsertClassCalendarWithJSONFormatIs(string multilineText)
+        {
+            var expected = JsonConvert.DeserializeObject<ClassCalendar>(multilineText);
+
+            var mockClassCalendarRepo = ScenarioContext.Current.Get<Mock<IClassCalendarRepository>>();
+            mockClassCalendarRepo.Verify(it => it.UpsertClassCalendar(It.Is<ClassCalendar>(actual =>
+                JsonConvert.SerializeObject(actual) == JsonConvert.SerializeObject(expected)
+            )));
+        }
+
+        [Then(@"System upsert UserActivity with JSON format is")]
+        public void ThenSystemUpsertUserActivityWithJSONFormatIs(string multilineText)
+        {
+            var expected = JsonConvert.DeserializeObject<UserActivity>(multilineText);
+
+            var mockUserActivityRepo = ScenarioContext.Current.Get<Mock<IUserActivityRepository>>();
+            mockUserActivityRepo.Verify(it => it.UpsertUserActivity(It.Is<UserActivity>(actual =>
+                JsonConvert.SerializeObject(actual) == JsonConvert.SerializeObject(expected)
+            )));
+        }
+
+        [Then(@"System upsert StudentKey with JSON format is")]
+        public void ThenSystemUpsertStudentKeyWithJSONFormatIs(string multilineText)
+        {
+            var expected = JsonConvert.DeserializeObject<StudentKey>(multilineText);
+            var mockStudentKeyRepo = ScenarioContext.Current.Get<Mock<IStudentKeyRepository>>();
+            mockStudentKeyRepo.Verify(it => it.UpsertStudentKey(It.Is<StudentKey>(actual =>
+                 JsonConvert.SerializeObject(actual) == JsonConvert.SerializeObject(expected)
+            )));
+        }
     }
 }
