02fdb9b1d64dfbc1048bc10fee5d884565f8d9e1
Sakul Jaruthanaset <miolynet@perfenterprise.com>
2016-05-02 19:21:52 +0700

    Create models & repository for update account profile complete.
    Signed-off-by: Sakul Jaruthanaset <miolynet@perfenterprise.com>

---
+++ WebAppSolution/src/MindSageWebJob/Models/UpdateUserProfileMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputeWebJobsSDKQueue.Models
+{
+    public class UpdateUserProfileMessage
+    {
+        #region Properties
+
+        public string UserProfileId { get; set; }
+        public string DisplayName { get; set; }
+        public string ProfileImageUrl { get; set; }
+
+        #endregion Properties
+    }
+}
