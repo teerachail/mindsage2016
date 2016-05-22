82b543d77e064cceae2f99da62a419b7911e2b18
Sakul Jaruthanaset <miolynet@perfenterprise.com>
2016-05-22 15:22:26 +0700

    Implement view for import Santiago's pages complete. (Just only view)
    Signed-off-by: Sakul Jaruthanaset <miolynet@perfenterprise.com>

---
+++ WebManagementPortal/WebManagementPortal/Controllers/ImportContentController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebManagementPortal.ViewModels;
+
+namespace WebManagementPortal.Controllers
+{
+    public class ImportContentController : Controller
+    {
+        // GET: ImportContent
+        public ActionResult Index()
+        {
+            var configInfo = new ImportContentViewModel
+            {
+                BaseURL = "base url",
+                HomePageURL = "home.html",
+                StorageInfo = new StorageInformation
+                {
+                    StorageBaseURL = "storage url",
+                    StorageKey = "storage key"
+                },
+                PagesURLs = new List<string>
+                {
+                    @"/what-is-mind-sage.html",
+                    @"/why-mind-sage.html",
+                    @"/how-to-mind-sage.html",
+                },
+                ReferenceFileURLs = new List<string>
+                {
+                    @"www.google.com/javascript.js",
+                    @"www.google.com/twitter/script.js",
+                },
+                ReplaceSections = new List<ReplaceSectionInformation>
+                {
+                    new ReplaceSectionInformation { Original = "A", ReplacedBy = "A1" },
+                    new ReplaceSectionInformation { Original = "B", ReplacedBy = "B1" },
+                    new ReplaceSectionInformation { Original = "C", ReplacedBy = "C1" },
+                    new ReplaceSectionInformation { Original = "D", ReplacedBy = "D1" },
+                }
+            };
+            return View(configInfo);
+        }
+
+        [HttpPost]
+        public ActionResult Index(ImportContentViewModel model)
+        {
+            model.PagesURLs = model.PagesURLs.Where(it => !string.IsNullOrEmpty(it) && !string.IsNullOrWhiteSpace(it)).ToList();
+            model.ReferenceFileURLs = model.ReferenceFileURLs.Where(it => !string.IsNullOrEmpty(it) && !string.IsNullOrWhiteSpace(it)).ToList();
+            model.ReplaceSections = model.ReplaceSections.Where(it => it != null 
+                && !string.IsNullOrEmpty(it.Original) && !string.IsNullOrWhiteSpace(it.Original)
+                && !string.IsNullOrEmpty(it.ReplacedBy) && !string.IsNullOrWhiteSpace(it.ReplacedBy))
+                .ToList();
+
+            return View(model);
+        }
+    }
+}
---
+++ WebManagementPortal/WebManagementPortal/ViewModels/ImportContentViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebManagementPortal.ViewModels
+{
+    public class ImportContentViewModel
+    {
+        [Required]
+        public string BaseURL { get; set; }
+        [Required]
+        public string HomePageURL { get; set; }
+        public List<string> PagesURLs { get; set; }
+        public List<string> ReferenceFileURLs { get; set; }
+        public List<ReplaceSectionInformation> ReplaceSections { get; set; }
+        public StorageInformation StorageInfo { get; set; }
+    }
+    public class ReplaceSectionInformation
+    {
+        public string Original { get; set; }
+        public string ReplacedBy { get; set; }
+    }
+    public class StorageInformation
+    {
+        [Required]
+        public string StorageBaseURL { get; set; }
+        [Required]
+        public string StorageKey { get; set; }
+    }
+}
